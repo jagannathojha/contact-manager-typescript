2cefd894497177910333942c9ba31eb63e3de971
Jagannath Ojha <ojha.jagann@gmail.com>
2024-12-14 17:45:12 +0545

    contact form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import ContactForm from "./components/ContactForm";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="main-container">
+        <ContactForm />
+      </div>
     </div>
   );
 };
